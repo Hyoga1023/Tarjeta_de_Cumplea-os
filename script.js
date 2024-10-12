1342285a4423ceca1ab9343e29361e5281edc2d2
Cesar Martinez <cesarmartinez99@gmail.com>
2024-10-12 06:43:15 -0500

    actualiza musica en la aplicación

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const backgroundMusic = document.getElementById('background-music');
 
     giftImage.addEventListener('click', function() {
-        // Mostrar la tarjeta al hacer clic en la imagen
         tarjeta.style.display = 'block';
 
-        // Lanzar confeti
         confetti({
             particleCount: 100,
             spread: 70,
             origin: { y: 0.6 }
         });
 
-        // Iniciar la música si está pausada
         if (backgroundMusic.paused) {
             backgroundMusic.play().catch(function(error) {
                 console.log('Autoplay was prevented:', error);
